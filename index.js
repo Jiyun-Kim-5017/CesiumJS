1ae551f4f5e04e937d7f64f17d039bc91e3ecf35
jiyun5017 <jo522115@gmail.com>
2023-11-27 17:34:09 +0900

    setting CesiumJS

---
+++ index.js
@@ -0,0 +1,4 @@
+let key =
+	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJhZTkzNDUzOC0zNzNjLTRiNjgtODEyNC03MGFkN2Q5NmJkYTkiLCJpZCI6MTgwOTMyLCJpYXQiOjE3MDEwNzMzMzh9.LChUlu3p74pFuj6kFCyLkdDZ--9ax2cttsopv1O4nb0";
+
+Cesium.Ion.defaultAccessToken = key;
